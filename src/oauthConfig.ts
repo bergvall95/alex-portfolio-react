9b5c7599dad3f2e2e0cb1fb9628496853af70d56
alex bergvall <bergvall95@gmail.com>
2022-08-14 20:59:40 -0500

    working on github auth, need to implement component and read up more on github OAuth docs

--- src/oauthConfig.ts
+++ src/oauthConfig.ts
@@ -9,3 +9,13 @@ export const getAuthorizeHref = (): string => {
   const redirectUri = process.env.REACT_APP_REDIRECT_URI;
   return `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token`;
 }
+
+const githubAuthEndpoint = "https://github.com/login/oauth/authorize";
+const githubScopes=['user'];
+
+export const getGithubAuthHref = (): string => {
+  const clientId = process.env.PORTFOLIO_GITHUB_CLIENT_ID;
+  const redirectUri = process.env.PORTFOLIO_REDIRECT_URI;
+
+  return `${githubAuthEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${githubScopes.join("%20")}&response_type=token`;
+}
